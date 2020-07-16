363ee91797eeeec970606e7419929c748ebb7c56
Jesse Pelzar <jessepelzar@gmail.com>
2020-07-16 15:17:14 -0400

    test3

--- scraper/views.py
+++ scraper/views.py
@@ -296,7 +296,7 @@ def get_user(user_id, user_info):
         return user_info
     #     # print(cookie)
 
- 
+    print(response_data)
     # user_url_data_dumps = json.dumps(response_data.text)
     user_url_data_loads = json.loads(response_data.text)
     # user_url_data_l_loads = user_url_data_loads.lower()
@@ -305,7 +305,7 @@ def get_user(user_id, user_info):
     # -------------------------
     print("----------------------------------")
     print(response_data)
-    
+
     print("location:")
     # print(user_url_data_loads['location'])
     print("----------------------------------")
