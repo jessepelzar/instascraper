e1acf26ac5a0086737e921c8ddf798de84d39fdb
Jesse Pelzar <jessepelzar@gmail.com>
2020-07-25 13:30:32 -0400

    get due date test 1

--- scraper/views.py
+++ scraper/views.py
@@ -534,7 +534,17 @@ def get_future_date(shortcode, tagwithnumber):
     # -------------------------
     # -------------------------
 
-   
+def dayOfYear(month, day, year):
+    date = '%s-%s-%s' % (year, month, day)
+    days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    d = list(map(int,date.split("-")))
+    if d[0] % 400 == 0:
+        days[2]+=1
+    elif d[0]%4 == 0 and d[0]%100!=0:
+        days[2]+=1
+    for i in range(1,len(days)):
+        days[i]+=days[i-1]
+    return days[d[1]-1]+d[2]
 
 def get_location(shortcode):
     r = ""
