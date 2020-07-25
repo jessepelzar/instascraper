3f2856b77c1679947dea1f0c78970bfd8d8697e4
Jesse Pelzar <jessepelzar@gmail.com>
2020-07-25 14:26:18 -0400

    expand user data test1

--- scraper/views.py
+++ scraper/views.py
@@ -447,7 +447,8 @@ def start_scraping(entry, choice, filename_r, tag_num_switch_r):
                             if str(tag_num_switch_r) == "true":
                                 print("tag switch true")
                                 future_date = get_future_date(shortcode, entryChosen)
-                                info.append(future_date)
+                                info.extend([future_date, entryChosen])
+                                
                         print(info)
                         print("--- %s seconds | User Time ---" % round(time.time() - start_time1, 2))
                         start_time2 = time.time()
