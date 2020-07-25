bbb75dd9cf9225bfdc2f6e4c6b847dbc215f643c
Jesse Pelzar <jessepelzar@gmail.com>
2020-07-24 23:32:21 -0400

    get due date test 1

--- scraper/views.py
+++ scraper/views.py
@@ -447,7 +447,7 @@ def start_scraping(entry, choice, filename_r, tag_num_switch_r):
                             if tag_num_switch_r is "true":
                                 print("tag switch true")
                                 future_date = get_future_date(shortcode, entryChosen)
-                                info.append(future_date)
+                                # info.append(future_date)
                         print(info)
                         print("--- %s seconds | User Time ---" % round(time.time() - start_time1, 2))
                         start_time2 = time.time()
